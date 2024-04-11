1a302d30711b02e2e32f5cd0e36d235364be0a25
Lorenzo Giacometti <lorenzo.giacometti@edu.unito.it>
2024-04-11 15:52:49 +0200

    path similarty, partially working

--- 2_WSD_1/libs.py
+++ 2_WSD_1/libs.py
@@ -7,85 +7,99 @@ import csv
 from nltk.corpus import wordnet as wn
 
 
+#MAX_DEPTH = max(max(len(hyp_path) for hyp_path in ss.hypernym_paths()) for ss in wn.all_synsets())
+MAX_DEPTH=20
+
 #Wu & Palmer similarity
 def wu_similarity(v1,v2):
     if(v1==v2):
         return 1.0
-    lcs = LCS(v1,v2)
+    lcs , _ = LCS(v1,v2)
     if(not lcs):
         return 0
-    #print(lcs[0].max_depth())
-    #wu = (2*(lcs[0].max_depth()+1))/(v1.max_depth()+v2.max_depth()+2)
     wu = (2*(depth(lcs[0])))/(depth(v1)+depth(v2))
     return round(wu,3)
 
 
 #Shortest Path similarity
-#def sp_similarity(v1,v2):
+def sp_similarity(v1,v2):
+    _ , len = LCS(v1,v2)
+    if len is None:
+        sp=0
+    else:
+        sp=2*MAX_DEPTH-len
+    return sp
 
 #Leak & Chod
 #def lch_similarity(v1,v2):
 
 def LCS(s1,s2):
-    s1_set = {str(s1)}
-    s2_set = {str(s2)}
+    #SE s1 e s2 sono uguali?
+    s1_dict = {str(s1):0}
+    s2_dict = {str(s2):0}
     s1_to_expand = [s1]
     s2_to_expand = [s2]
     matches=[]
+    level=1
     while not matches and (s1_to_expand or s2_to_expand):
+        hyperonism_1=[]
         for s1_ext in s1_to_expand:
-            if(str(s1_ext) in s2_set):
-                matches.append(s1_ext)#DA METTERE FUORI DAL FOR
+            if(str(s1_ext) in s2_dict):
+                matches.append(s1_ext)
             else:
                 for s1_hypr in s1_ext.hypernyms():
-                    s1_set.add(str(s1_hypr))
-                    s1_to_expand.append(s1_hypr)#DA METTERE FUORI DAL FOR
-            s1_to_expand.remove(s1_ext)#DA METTERE FUORI DAL FOR
+                    s1_dict.update({str(s1_hypr):level})
+                    hyperonism_1.append(s1_hypr)
+        s1_to_expand=hyperonism_1
     
-    
+        hyperonism_2=[]
         for s2_ext in s2_to_expand:
-            if(str(s2_ext) in s1_set):
-                matches.append(s2_ext)#DA METTERE FUORI DAL FOR
+            if(str(s2_ext) in s1_dict):
+                matches.append(s2_ext)
             else:
                 for s2_hypr in s2_ext.hypernyms():
-                    s2_set.add(str(s2_hypr))
-                    s2_to_expand.append(s2_hypr)#DA METTERE FUORI DAL FOR
-            s2_to_expand.remove(s2_ext)#DA METTERE FUORI DAL FOR
+                    s2_dict.update({str(s2_hypr):level})
+                    hyperonism_2.append(s2_hypr)
+        s2_to_expand=hyperonism_2
+
+        level+=1
     
-    if (matches):
+    if (len(matches) > 1): #se c'è più di un match allora 
         #min(common_hypernyms, key=lambda x: x.shortest_path_distance(synset1) + x.shortest_path_distance(synset2))
         min=matches[0]
         for actual in matches:
-            value_min=s1.shortest_path_distance(min) + s2.shortest_path_distance(min)
-            value_actual=s1.shortest_path_distance(actual) + s2.shortest_path_distance(actual)
+            value_min=s1_dict.get(str(min))+s2_dict.get(str(min))
+            value_actual=s1_dict.get(str(actual))+s2_dict.get(str(actual))
             if(value_actual<value_min):
                 min=actual
-        return [min]
+        return [min] , (s1_dict.get(str(min))+s2_dict.get(str(min)))
+    elif (len(matches)==1):#se c'è solo un mathc
+        return matches , (s1_dict.get(str(matches[0]))+s2_dict.get(str(matches[0])))
     else:
-        return []
-    
+        return [], None
 
 def depth(v1):
     count = 1
     hypernym = v1
-    #print(hypernym)
     while str(hypernym.hypernyms()) != "[]":
-        #print(str(hypernym))
         hypernym = hypernym.hypernyms()[0]
-        #print(hypernym)
         count=count+1
     return count
 
-
-#tiger=wn.synsets('tiger')
-#cat=wn.synsets('cat')
+tiger=wn.synsets('cock')
+cat=wn.synsets('pussy')
 #print(tiger)
 #print(cat)
 
-#for s1 in tiger:
-#    for s2 in cat:
-#        wu_similarity(s1,s2)
-        
+print(tiger[0].shortest_path_distance(cat[0]))
+_ , spd= LCS(tiger[0],cat[0])
+print(spd)
+
+
+
+#print(LCS(tiger[1],cat[0]))
+#print(tiger[1].lowest_common_hypernyms(cat[0]))
+
 
         
 
