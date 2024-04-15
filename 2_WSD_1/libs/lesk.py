61de02f2c5112255b5928944f00c6d8a9726796d
DH <danielhaures01@gmail.com>
2024-04-15 12:11:47 +0200

    Gemini integration

--- 2_WSD_1/libs/lesk.py
+++ 2_WSD_1/libs/lesk.py
@@ -3,9 +3,42 @@ import random
 from nltk.corpus import wordnet as wn
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
+import google.generativeai as genai
+
+##GEMINI SET-UP
+genai.configure(api_key="AIzaSyBjFgxtyUvVOt8SXOGlX2vRCogyYfxm4Ik")
+generation_config = {
+  "temperature": 0.7,
+  "top_p": 0.9,
+  "top_k": 1,
+  "max_output_tokens": 2048,
+}
+
+safety_settings = [
+  {
+    "category": "HARM_CATEGORY_HARASSMENT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_HATE_SPEECH",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+]
+model = genai.GenerativeModel(model_name="gemini-1.0-pro",
+                              generation_config=generation_config,
+                              safety_settings=safety_settings)
+
+
+
 lemmatizer = WordNetLemmatizer()
-
-
 PUNCTUATION = string.punctuation
 #STOPWORDS = set(line.strip() for line in open('data\\stop_words_FULL.txt'))
 STOPWORDS = set(stopwords.words('english'))
@@ -16,7 +49,8 @@ def lesk_algorithm(word, sentence):
     max_overlap = 0
     context = cleaning(sentence)
     for sense in wn.synsets(word):
-        signature = get_signature(sense)
+        #signature = get_signature(sense)
+        signature = gemini_get_signature(sense)
         overlap = get_overlap(signature, context) 
         if overlap>max_overlap:
             max_overlap=overlap
@@ -40,9 +74,30 @@ def cleaning(sentence):
 def get_signature(sense):
      return cleaning(sense.definition())
 
+def gemini_get_signature(word, sense):
+  prompt_parts = ["write some examples which contain the word ",word," defined as ",sense.definition()]
+  response = model.generate_content(prompt_parts)
+  return cleaning(response.text)
+
 #counting the number of shared words
 def get_overlap(signature,context):
  sig=set(signature)
  con=set(context)
  intersection = con.intersection(sig)
  return len(intersection)
+
+
+def gemini_lesk_algorithm(word, sentence):
+    best_sense = wn.synsets(word)[0]
+    max_overlap = 0
+    context = cleaning(sentence)
+    for sense in wn.synsets(word):
+        signature = gemini_get_signature(word,sense)
+        overlap = get_overlap(signature, context) 
+        if overlap>max_overlap:
+            max_overlap=overlap
+            best_sense=sense
+    return best_sense
+
+
+print(gemini_lesk_algorithm("bed","I am going to bed early because I'm tired"))
