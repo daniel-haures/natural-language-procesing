43c82f268945b03665df6f2bcda0c1d2188ef5e5
Lorenzo Giacometti <lorenzo.giacometti@edu.unito.it>
2024-04-15 11:36:32 +0200

    cleaned

--- 2_WSD_1/libs/lesk.py
+++ 2_WSD_1/libs/lesk.py
@@ -1,22 +1,22 @@
 import string
 import random
 from nltk.corpus import wordnet as wn
+from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 lemmatizer = WordNetLemmatizer()
 
 
 PUNCTUATION = string.punctuation
-STOPWORDS = set(line.strip() for line in open('data\\stop_words_FULL.txt'))
+#STOPWORDS = set(line.strip() for line in open('data\\stop_words_FULL.txt'))
+STOPWORDS = set(stopwords.words('english'))
 
 
 def lesk_algorithm(word, sentence):
     best_sense = wn.synsets(word)[0]
     max_overlap = 0
     context = cleaning(sentence)
-    #print(context)
     for sense in wn.synsets(word):
         signature = get_signature(sense)
-        #print(signature)
         overlap = get_overlap(signature, context) 
         if overlap>max_overlap:
             max_overlap=overlap
@@ -25,7 +25,6 @@ def lesk_algorithm(word, sentence):
 
 
 # clean the sentence from punctuation and stopwords
-#eliminare i nomi propri
 def cleaning(sentence):
     sentence = sentence.lower()
     for p in PUNCTUATION:
@@ -34,7 +33,7 @@ def cleaning(sentence):
     sentence=[i for i in sentence if i not in STOPWORDS]
     lemmatized_sentece=[lemmatizer.lemmatize(i) for i in sentence]
     #lemmatizazione
-    return sentence
+    return lemmatized_sentece
 
 
 #get all the set of words in the gloss and examples of sense
@@ -47,7 +46,3 @@ def get_overlap(signature,context):
  con=set(context)
  intersection = con.intersection(sig)
  return len(intersection)
-
-
-#print(cleaning("foxes and Juliet I'm hoping to go jogging I haven't eaten in a while where is everybody going"))
-#print(lesk_algorithm("bed","love to bed"))
